80a6e6e3c5174e7f34007fa297c952dc95e529c5
Ufuk Balaban <ufukkblbnn@gmail.com>
2022-07-24 15:35:51 +0300

    updates

---
+++ index.js
@@ -0,0 +1,4 @@
+console.log("test!");
+
+//**npm i nodemon -g** => it monitors your files and when you save it automatically restarts the server so we're not always have to restart
+//**npm init **/
